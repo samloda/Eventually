945320ee10f988abb9f071860105a1a66419b057
samloda <samuel.loda@loop.colum.edu>
2015-05-12 06:44:28 -0500

    Eventually v12.3.2

---
+++ Eventually/Assets/References/AudioLab/SoundScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundScript : MonoBehaviour {
+
+	public AudioClip audioClip;
+
+	private AudioSource audioSource;
+
+	// Use this for initialization
+	void Start () {
+		audioSource = this.gameObject.AddComponent<AudioSource>();
+		audioSource.clip = audioClip;
+		audioSource.PlayOneShot (audioClip);
+	}
+}
